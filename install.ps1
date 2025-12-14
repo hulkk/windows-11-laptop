b0669b31b61e766350b3458069a9c86dcb024b3b
Timo <timo@hulkkonen.fi>
2025-12-14 18:35:29 +0200

    update

--- install.ps1
+++ install.ps1
@@ -1,4 +1,5 @@
-winget install --id Brave.Brave
+winget install --exact --id Brave.Brave
 winget install --name "Lenovo Commercial Vantage"
-winget install --id microsoft.powershell
+winget install --exact --id microsoft.powershell
 winget install --name "Harden System Security"
+winget install --exact --id RustDesk.RustDesk
